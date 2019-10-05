f18935e55632c99dc0c293aedc73543d6a651218
AndyOmondi17 <andyomondi17@gmail.com>
2019-10-05 10:58:20 +0300

    add user method to return userName

--- src/app/git-search-form/git-search-form.component.ts
+++ src/app/git-search-form/git-search-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-git-search-form',
@@ -6,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./git-search-form.component.css']
 })
 export class GitSearchFormComponent implements OnInit {
+  @Output () emitUsers = new EventEmitter<any>()
+
+  userName:string;
+
+  user(){
+     this.emitUsers.emit(this.userName);
+  }
 
   constructor() { }
 
